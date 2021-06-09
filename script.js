8aa7845db2a3b2bc74c5ad8c003d010d79a9bfba
BKAUTO <wubingkun1603@gmail.com>
2021-06-09 20:16:05 +0000

    done

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function compute()
 {
     var principal = document.getElementById("principal").value;
 
-    if (principal <= 0) {
+    if (principal <= 0 || principal=='') {
         alert("Enter a positive number!");
         document.getElementById("principal").focus();
         return false;
@@ -14,7 +14,7 @@ function compute()
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear() + parseInt(years);
 
-    document.getElementById("result").innerHTML = "If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
+    document.getElementById("result").innerHTML = "If you deposit "+"<mark>"+principal+"</mark>"+",\<br\>at an interest rate of "+"<mark>"+rate+"</mark>"+"%\<br\>You will receive an amount of "+"<mark>"+interest+"</mark>"+",\<br\>in the year "+"<mark>"+year+"</mark>"+"\<br\>";
     document.getElementById("principal").focus();
     return true;
 }
